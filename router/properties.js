ace162dc336b0847353491f929582f9ef6c1fe08
Gastón Axel Lacuesta <98782312+gaxelac0@users.noreply.github.com>
2023-10-30 13:08:49 -0300

    propertiesEndpoints (#5)
    * fix updateProprety + TODO! delete property
    
    * v1/properties DELETE + validaciones de todos los campos en body y params

--- router/properties.js
+++ router/properties.js
@@ -1,13 +1,16 @@
 const { check } = require("express-validator");
 const { validateField } = require("../middlewares/fieldValidator.js");
 const checkAuth  = require("../middlewares/auth.js").checkAuth;
+const propertiesCtrl = require("../controllers/properties.js");
 
 const { Router } = require("express");
+
+const constants = require("../common/constants.js");
 
 const router = Router();
 router.use(require("../middlewares/response").jsonExtra);
 
-const propertiesCtrl = require("../controllers/properties.js");
+
 
 
 /*---------- Public Routes ----------*/
@@ -25,9 +28,32 @@ router.get("/owned", propertiesCtrl.getOwnedProperties);
 // agregar property
 router.post("/", 
 [
-  // check("nombre", "El nombre es obligatorio").not().isEmpty(),
-  // check("descripcion", "La descripcion es obligatorio").not().isEmpty(),
-  // check("tipo", "El tipo es obligatorio").not().isEmpty(),
+   check("title", "title es obligatorio").notEmpty(),
+   check("description", "description es obligatoria").notEmpty(),
+   check("propertyType", "propertyType es obligatorio ").notEmpty(),
+   check("propertyType", "propertyType debe ser Casa, Departamento, PH, Terreno o Local Comrecial").isIn(Object.values(constants.PropertyTypeEnum)),
+   check("position", "position debe ser Frente, Contrafrente, Interno o Lateral").optional().isIn(Object.values(constants.PositionEnum)),
+   check("orientation", "orientation debe ser N, S, E, O, SE, SO, NO, o NE").optional().isIn(Object.values(constants.OrientationEnum)),
+
+
+   // Validacion contract_types array
+   check("contract_types", "contract_types debe ser un array de objetos").optional().isArray(),
+
+   check("contract_types.*.contractType", "contractType es obligatorio para dar de alta un tipo de contrato de propiedad.").notEmpty(),
+   check("contract_types.*.contractType", "contractType debe ser un tipo valido (e.g, Venta, Alquiler, Temporada).").isIn(Object.values(constants.ContractTypeEnum)),
+
+   
+
+   check("contract_types.*.price", "price es obligatorio para dar de alta un tipo de contrato de propiedad.").notEmpty(),
+   check("contract_types.*.price", "price debe ser un valor de coma flotante.").isFloat(),
+
+   check("contract_types.*.expPrice", "expPrice es obligatorio para dar de alta un tipo de contrato de propiedad.").notEmpty(),
+   check("contract_types.*.expPrice", "expPrice debe ser un valor de coma flotante.").isFloat(),
+
+   check("contract_types.*.currency", "currency es obligatorio para dar de alta un tipo de contrato de propiedad.").notEmpty(),
+   check("contract_types.*.currency", "currency debe ser un tipo valido (e.g, AR$, US$).").isIn(Object.values(constants.CurrencyTypeEnum)),
+
+
   validateField,
 ],
 checkAuth,
@@ -37,10 +63,27 @@ propertiesCtrl.addProperty);
 // actualizar property
 router.patch("/", 
 [
-  // check("propertyId", "El propertyId es obligatorio").not().isEmpty(),
-  // check("nombre", "El nombre es obligatorio").not().isEmpty(),
-  // check("descripcion", "La descripcion es obligatorio").not().isEmpty(),
-  // check("tipo", "El tipo es obligatorio").not().isEmpty(),
+
+  check("propertyType", "propertyType debe ser Casa, Departamento, PH, Terreno o Local Comrecial").optional().isIn(Object.values(constants.PropertyTypeEnum)),
+  check("position", "position debe ser Frente, Contrafrente, Interno o Lateral").optional().isIn(Object.values(constants.PositionEnum)),
+  check("orientation", "orientation debe ser N, S, E, O, SE, SO, NO, o NE").optional().isIn(Object.values(constants.OrientationEnum)),
+
+
+  // Validacion contract_types array
+  check("contract_types", "contract_types debe ser un array de objetos").optional().isArray(),
+
+  check("contract_types.*.contractType", "contractType es obligatorio para dar de alta un tipo de contrato de propiedad.").notEmpty(),
+  check("contract_types.*.contractType", "contractType debe ser un tipo valido (e.g, Venta, Alquiler, Temporada).").isIn(Object.values(constants.ContractTypeEnum)),
+
+  check("contract_types.*.price", "price es obligatorio para dar de alta un tipo de contrato de propiedad.").notEmpty(),
+  check("contract_types.*.price", "price debe ser un valor de coma flotante.").isFloat(),
+
+  check("contract_types.*.expPrice", "expPrice es obligatorio para dar de alta un tipo de contrato de propiedad.").notEmpty(),
+  check("contract_types.*.expPrice", "expPrice debe ser un valor de coma flotante.").isFloat(),
+
+  check("contract_types.*.currency", "currency es obligatorio para dar de alta un tipo de contrato de propiedad.").notEmpty(),
+  check("contract_types.*.currency", "currency debe ser un tipo valido (e.g, AR$, US$).").isIn(Object.values(constants.CurrencyTypeEnum)),
+
   validateField,
 ],
 checkAuth,
