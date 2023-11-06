dc3eb026881b50ea22b67783465d8c383d846c43
Gastón Axel Lacuesta <gaxelac@outlook.com>
2023-11-06 19:22:48 -0300

    access controls

--- server.js
+++ server.js
@@ -19,6 +19,28 @@ class Server {
     this.app.use(express.static(path.resolve(__dirname, "../public")));
     // Cors
     this.app.use(cors());
+
+    // Add headers before the routes are defined
+    app.use(function (req, res, next) {
+
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', '*');
+
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      //res.setHeader('Access-Control-Allow-Credentials', true);
+
+      // Pass to next layer of middleware
+      next();
+    });
+
+
     // Body Parser
     this.app.use(express.json());
     this.app.use(formData.parse());
